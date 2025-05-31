c499d986110ad81146e79ff653537cd4f7dcc196
mahmoud sami <89742294+mahmoud-riz@users.noreply.github.com>
2025-05-31 04:19:52 +0300

    Update TestListener.java

--- src/main/java/com/automation/listeners/TestListener.java
+++ src/main/java/com/automation/listeners/TestListener.java
@@ -6,9 +6,6 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-/**
- * Test listener for tracking test execution and failures
- */
 @Slf4j
 public class TestListener implements ITestListener {
     
